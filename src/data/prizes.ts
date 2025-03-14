cb5397e76701b866c795fb67f8fe72718c820cdc
Fernando <fernandoasfilho74@gmail.com>
2025-03-14 13:09:02 -0300

    aws reclamando deles

--- src/data/prizes.ts
+++ src/data/prizes.ts
@@ -2,14 +2,6 @@ import bag from "../assets/prizes/bag.png";
 import headset from "../assets/prizes/headset.png";
 import plastic_bottle from "../assets/prizes/plastic-bottle.png";
 import mug from "../assets/prizes/mug.png";
-import shirt from "../assets/prizes/shirt.png";
-import mouse from "../assets/prizes/mouse.png";
-import teclado from "../assets/prizes/keyboard.png";
-import powerbank from "../assets/prizes/powerbank.png";
-import hat from "../assets/prizes/hat.png";
-import glasses from "../assets/prizes/glasses.png";
-import clock from "../assets/prizes/clock.png";
-import microphone from "../assets/prizes/microphone.png"; // Para o caso de não ganhar nada
 
 const prizes = [
   { name: "Não Foi dessa Vez", image: bag },
